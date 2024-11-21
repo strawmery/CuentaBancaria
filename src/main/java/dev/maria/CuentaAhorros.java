83051e38a1ca8551d5f4f23c68f350b339619ed9
Maria Garcia <mgarciasierragrsr@gmail.com>
2024-11-21 12:34:16 +0100

    method retirar done

--- src/main/java/dev/maria/CuentaAhorros.java
+++ src/main/java/dev/maria/CuentaAhorros.java
@@ -19,7 +19,13 @@ public class CuentaAhorros extends Cuenta {
 
     @Override
     public double  retirar(double cantidad) {
-
+        if(isActiva() && cantidad <= getSaldo()){
+            double nuevoSaldo = getSaldo() - cantidad;
+            setSaldo(nuevoSaldo);
+            setTransacciones(getTransacciones()+1);
+            return getSaldo();
+        }
+            return 0;
     }
 
     @Override
